0c98bf7c3b4da9717f5f0cf35fefa6cd9439b3ec
tryggos16 <tryggvi.bogason@gmail.com>
2022-04-26 21:11:09 +0000

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/destini_ntv/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.destini_ntv
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Destini());
+
+class Destini extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: const StoryPage(),
+    );
+  }
+}
+
+
+class  StoryPage extends StatefulWidget {
+  const StoryPage ({Key? key}) : super(key: key);
+
+  @override
+  _StoryPageState createState() => _StoryPageState();
+}
+
+class _StoryPageState extends State<StoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+
+        //TODO: Step 1 - Add background.png to this Container as a background image.
+        padding: const EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
+        constraints: const BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              const Expanded(
+                flex: 12,
+                child: Center(
+                  child: Text(
+                    //TODO: Step 10 - use the storyBrain to get the first story title and display it in this Text Widget.
+                    'Story text will go here.',
+                    style: TextStyle(
+                      fontSize: 25.0,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  color: Colors.red,
+                  child: TextButton(
+                    onPressed: () {
+                      //Choice 1 made by user.
+                      //TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
+                    },
+                    child: const Text(
+                      //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
+                      'Choice 1',
+
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Expanded(
+                flex: 2,
+                //TODO: Step 26 - Use a Flutter Visibility Widget to wrap this FlatButton.
+                //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
+                child: Container(
+                  color: Colors.blue,
+                  child: TextButton(
+                    onPressed: () {
+                      //Choice 2 made by user.
+                      //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
+                    },
+                    child: const Text(
+                      //TODO: Step 14 - Use the storyBrain to get the text for choice 2.
+                      'Choice 2',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
